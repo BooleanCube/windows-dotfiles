29813fdf7153f8bbb9d7f78593118b0b845b1bc8
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2021-08-19 12:46:48 -0400

    Add files via upload

---
+++ AppData/Local/nvim-data/plugged/nvim-treesitter/queries/elixir/injections.scm
@@ -0,0 +1,20 @@
+(comment) @comment
+
+; TODO: re-add when markdown is added
+; (heredoc
+;   (heredoc_content) @markdown)
+
+((sigil
+   (sigil_start) @_start
+   (sigil_content) @regex)
+ (#match? @_start "~(r|R)[/</\\\"[({|]"))
+
+((sigil
+   (sigil_start) @_start
+   (sigil_content) @surface)
+ (#eq? @_start "~F\"\"\""))
+
+((sigil
+   (sigil_start) @_start
+   (sigil_content) @heex)
+ (#eq? @_start "~H\"\"\""))
